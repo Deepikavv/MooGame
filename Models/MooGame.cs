c18553754fb3b15d3ea95ea5bc91c3b7212fa64d
Deepika Vaidya Vittal <deepika.vaidya.vittal@iths.se>
2022-12-18 23:42:12 +0100

    Created ClientLayer and added IMooGame to interface

---
+++ Models/MooGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooGame.Models
+{
+    public class MooGame
+    {
+        public string GameType { get; set; }
+        public string Goal { get; set; }
+        public int GoalLength { get; set; }
+        public int MaxGoalLength { get; set; }
+        public int MaxRange { get; set; }
+        public string Guess { get; set; }
+        public string TargetBulls { get; set; }
+        public int NoOfGuesses { get; set; }
+        public string PlayerName { get; set; }
+
+        public PlayerData playerData = new PlayerData();
+    }
+}
